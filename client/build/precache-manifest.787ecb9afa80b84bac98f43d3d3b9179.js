6220fe88972ef426ae3ea99270c5b1d97d025506
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-03-08 14:37:33 +0200

    production - build

---
+++ client/build/precache-manifest.787ecb9afa80b84bac98f43d3d3b9179.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e4a6f008c76d6f548ea8a3e8ef8766e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf920755d3b17864e0bf",
+    "url": "/static/css/main.4187a5f6.chunk.css"
+  },
+  {
+    "revision": "1224e9f809dff3239121",
+    "url": "/static/js/2.870ca22c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.870ca22c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf920755d3b17864e0bf",
+    "url": "/static/js/main.ee8de326.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
