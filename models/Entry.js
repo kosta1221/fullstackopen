1f52eb7894bf8e5e2ee77eafe1c2cc752be7c25e
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-03-10 01:33:26 +0200

    Exercises 3.15, 3.16, 3.17

--- models/Entry.js
+++ models/Entry.js
@@ -23,4 +23,12 @@ const entrySchema = new mongoose.Schema({
 	number: String,
 });
 
+entrySchema.set("toJSON", {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString();
+		delete returnedObject._id;
+		delete returnedObject.__v;
+	},
+});
+
 module.exports = mongoose.model("Entry", entrySchema);
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static("./client/build"));
 
 app.use(express.json());
 
-// returns a hardcoded list of phonebook entries from /api/persons
+// GET route to /api/persons returns a list of phonebook entries from MongoDB database
 app.get("/api/persons", (req, res) => {
 	Entry.find({}).then((entries) => {
 		res.status(200).json(entries);
@@ -32,18 +32,18 @@ app.get("/info", (req, res) => {
 });
 
 // GET route to /api/persons/:id returns info about the person with that id
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
 	const { id } = req.params;
 
-	Entry.findById(id).then((note) => {
-		res.json(note);
-	});
-
-	/* if (foundEntry) {
-		res.status(200).json(foundEntry);
-	} else {
-		res.status(404).end();
-	} */
+	Entry.findById(id)
+		.then((entry) => {
+			if (entry) {
+				res.json(entry);
+			} else {
+				res.status(404).end();
+			}
+		})
+		.catch((error) => next(error));
 });
 
 // POST route to /api/persons adds a new entry
@@ -58,11 +58,6 @@ app.post("/api/persons", (req, res) => {
 		return res.json({ error: "Entry must have a phone number!" });
 	}
 
-	/* if (phonebookEntries.map((entry) => entry.name).includes(req.body.name)) {
-		console.log("error: That name already exists!");
-		return res.json({ error: "That name already exists!" });
-	} */
-
 	const newEntry = new Entry({ name: req.body.name, number: req.body.number });
 	newEntry.save().then((savedEntry) => {
 		res.json(savedEntry);
@@ -72,23 +67,32 @@ app.post("/api/persons", (req, res) => {
 // DELETE route to /api/persons/:id deletes an entry by id
 app.delete("/api/persons/:id", (req, res) => {
 	const { id } = req.params;
-	console.log(typeof id);
-	const lengthBeforeAttemptedDeletion = phonebookEntries.length;
 
-	phonebookEntries = phonebookEntries.filter((entry) => entry.id !== +id);
-
-	if (lengthBeforeAttemptedDeletion === phonebookEntries.length + 1) {
-		res.status(204).end();
-	} else {
-		res.status(404).end();
-	}
+	Note.findByIdAndRemove(id)
+		.then((result) => {
+			response.status(204).end();
+		})
+		.catch((error) => next(error));
 });
 
-function getRandomInt(min, max) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+const unknownEndpoint = (request, response) => {
+	return response.status(404).send({ error: "unknown endpoint" });
+};
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message);
+
+	if (error.name === "CastError") {
+		return response.status(400).send({ error: "invalid id format!" });
+	}
+
+	next(error);
+};
+
+app.use(errorHandler);
 
 app.listen(() => {
 	app.listen(PORT, () => {
